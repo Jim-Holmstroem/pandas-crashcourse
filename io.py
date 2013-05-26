ef90678f84f334643c73bc245e8792cb548eab9f
Jim Holmström <jim.holmstroem@gmail.com>
2013-05-26 10:45:47 +0200

    MOOOAORRgit add io.py :D::D:D

--- io.py
+++ io.py
@@ -3,6 +3,7 @@ import sqlite3
 
 import matplotlib.pyplot as plt
 
+import numpy as np
 import pandas as pd
 from pandas.io import sql
 
@@ -17,5 +18,37 @@ data.index = pd.DatetimeIndex(data.index) #parse_dates fix
 
 print(data.ix['2013-02-01':'2013-02-28'])
 print(data.open)
+print(data[['open', 'close']])
 
+data['diff'] = pd.Series(data['close']-data['open'], index=data.index)
 
+#GROUPBY
+groups = data.groupby('token')
+
+#AGGREGATIONS
+print(groups.mean())
+print(groups['close'].aggregate(
+    {
+        'minimum':  np.mean,
+        'variance': np.var,
+        'mean':     np.mean,
+    }
+))
+print(groups.aggregate(
+    [
+        np.mean,
+        np.var
+    ]
+))
+
+#RENDERING
+
+data[['open', 'close']].hist()
+plt.figure()
+data.boxplot()
+pd.tools.plotting.scatter_matrix(data, diagonal='kde')
+pd.tools.plotting.lag_plot(data['close'])
+pd.tools.plotting.autocorrelation_plot(groups.get_group('GOOG')['close'])
+pd.rolling_mean(groups.get_group('GOOG')[['close','open']], 3, 3).plot()
+groups.get_group('GOOG')[['close','open']].plot()
+plt.show()
